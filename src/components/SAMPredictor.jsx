c07bfd406cecbfd184e48ce504990be0907c0a5e
Simone <1024masi@gmail.com>
2025-01-10 15:59:27 +0100

    works

--- src/components/SAMPredictor.jsx
+++ src/components/SAMPredictor.jsx
@@ -13,7 +13,7 @@ import {
 
 const { Title, Text } = Typography;
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://0.0.0.0:5001';
+const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5001';
 
 
 const SAMPredictor = () => {
